f68d9c3485ece3ea19fa3f650407c1bae88bc451
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-06-16 00:46:33 +0530

    Error

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,11 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-
+    entry: './src/client/index.js',
+    node: {
+        crypto: 'empty',
+        tls: 'empty',
+        net: 'empty',
+        fs: 'empty',
+        },
 }
